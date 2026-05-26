f4d4eaa772a5451f8f7c2b75eefb00f86bc2fff5
LegoBro <42771605+LegoBro@users.noreply.github.com>
2026-05-26 13:59:25 -0500

    Bash

--- Pi/webserver.py
+++ Pi/webserver.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask import render_template, send_from_directory
+import os
 
 from utils import get_config
+
 
 cfg = get_config()
 
@@ -10,5 +13,19 @@ app = Flask(__name__)
 def index():
     return 'Hello world'
 
+@app.route('/listoffiles')
+def list_of_files():
+     filenames=os.listdir(cfg["recording"]["output_dir"])
+     return render_template('contents.html', files=filenames )
+
+@app.route('/listoffiles/<path:filename>')
+def read_file(filename):
+    return send_from_directory(os.path.abspath(cfg["recording"]["output_dir"]), filename, as_attachment=False)
+#here for attachment i went with flase otherwise it gonna download all the contents of the files
+
 if __name__ == '__main__':
     app.run(debug=True, host=cfg["web"]["host"], port=cfg["web"]["port"])
+
+
+# Credit:
+# https://medium.com/@venkatalakshmiundamatla97/flask-tutorial-list-and-view-files-from-a-directory-using-jinja2-templates-48d4c2e620e3
