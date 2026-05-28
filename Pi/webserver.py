2c22513f509b0d752fe106ae0bf264c37a4a17ea
LegoBro <zayincalvert@gmail.com>
2026-05-28 09:01:19 -0500

    Update webserver.py

--- Pi/webserver.py
+++ Pi/webserver.py
@@ -37,7 +37,7 @@ def read_file(filename):
 
 if __name__ == '__main__':
     app.run(debug=True, host=cfg["web"]["host"], port=cfg["web"]["port"])
-    logger.info(f"Starting webserver on {cfg["web"]}:{cfg["port"]}")
+    logger.info(f"Starting webserver on {cfg["web"]["host"]}:{cfg["web"]["port"]}")
 
 
 # Credit:
